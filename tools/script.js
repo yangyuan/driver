24320acd3ebcce161e9700be6bbb4e7d2f85392a
Yang Yuan <yangyuan@users.noreply.github.com>
2018-11-10 02:32:49 -0800

    Save

--- tools/script.js
+++ tools/script.js
@@ -13,21 +13,36 @@ app.factory('videosFactory', ['$resource', function ($resource) {
 
 app.controller('VideosController', ['$scope', '$http', '$interval', 'videosFactory', function ($scope, $http, $interval, videosFactory) {
     let vid = document.getElementById("player");
+
+    $scope.colors = [
+        '#CCCCCC',
+        '#007BFF',
+        '#FF4000',
+        '#28a745',
+        '#FFFF00',
+        '#00FFFF',
+        '#800080',
+        '#008080',
+        '#FF00FF',
+        '#FFA500',
+    ];
+
     $scope.isEditMode = false;
 
     $scope.videos = [];
-    $scope.currentVideo = {};
+    $scope.currentVideo = {duration: -1};
     $scope.currentStatus = '0';
-    $scope.currentDuration = -1;
     $scope.currentActionsMap = new Map();
+    $scope.currentActionsCache = [];
+
 
     vid.addEventListener('loadedmetadata', function () {
-        if ($scope.currentDuration < 0) {
+        if ($scope.currentVideo.duration < 0) {
             $scope.currentStatus = '0';
-            $scope.currentDuration = vid.duration;
-            $scope.currentActionsMap = initializeActions($scope.currentDuration);
+            $scope.currentVideo.duration = Math.round(vid.duration * 10) / 10;
+            $scope.currentActionsMap = initializeActions(vid.duration);
             $scope.$apply();
-            console.log($scope.currentDuration);
+            console.log($scope.currentVideo.duration);
         }
     });
 
@@ -41,9 +56,13 @@ app.controller('VideosController', ['$scope', '$http', '$interval', 'videosFacto
         if (vid.readyState === 4 && vid.paused === false) {
             let frame_id = Math.round(vid.currentTime * 10);
             if ($scope.isEditMode) {
-                // console.log(vid.currentTime);
-                // console.log(parseInt($scope.currentStatus));
-                $scope.currentActionsMap.set(frame_id, parseInt($scope.currentStatus));
+                let value_old = $scope.currentActionsMap.get(frame_id)
+                let value_new = parseInt($scope.currentStatus)
+
+                if (value_old !== value_new) {
+                    $scope.currentActionsMap.set(frame_id, value_new);
+                    $scope.currentActionsCache = compressToCache($scope.currentActionsMap, $scope.currentVideo.duration);
+                }
             } else {
                 $scope.currentStatus = $scope.currentActionsMap.get(frame_id).toString();
             }
@@ -51,22 +70,31 @@ app.controller('VideosController', ['$scope', '$http', '$interval', 'videosFacto
     }, 25);
 
     $scope.onSave = function () {
-        $scope.currentVideo.actions = compressActions($scope.currentActionsMap, $scope.currentDuration);
+        $scope.currentVideo.actions = compressActions($scope.currentActionsMap, $scope.currentVideo.duration);
         console.log($scope.currentVideo);
 
         videosFactory.update($scope.currentVideo);
     };
 
+    $scope.onActionSelect = function () {
+        $scope.isEditMode = true;
+        $('#checkboxEditMode').addClass('active')
+    };
+
     $scope.onVideoClick = function (video) {
-        $scope.currentVideo = {};
         $scope.currentStatus = '0';
-        $scope.currentDuration = -1;
         $scope.currentActionsMap = new Map();
 
+        if (video.duration === undefined) {
+            video.duration = -1
+        }
+
         $scope.currentVideo = video;
+
+
         if ($scope.currentVideo.actions != null && video.duration > 0) {
-            $scope.currentDuration = video.duration;
             $scope.currentActionsMap = extractActions(video.actions, video.duration);
+            $scope.currentActionsCache = compressToCache($scope.currentActionsMap, $scope.currentVideo.duration);
 
             if ($scope.currentActionsMap.has(0)) {
                 $scope.currentStatus = $scope.currentActionsMap.get(0).toString();
@@ -89,8 +117,6 @@ app.controller('VideosController', ['$scope', '$http', '$interval', 'videosFacto
         }
 
         for (let i = 0; i <= length; i++) {
-            let seek = i / 10;
-            let action = 0;
             if (compressedActionsMap.has(i)) {
                 lastAction = compressedActionsMap.get(i);
             }
@@ -118,11 +144,38 @@ app.controller('VideosController', ['$scope', '$http', '$interval', 'videosFacto
         return actionsList;
     }
 
+    function compressToCache(actionsMap, duration) {
+        let actionsList = [];
+        let length = Math.ceil(duration * 10);
+        let lastIndex = 0;
+        let lastAction = -1;
+        for (let i = 0; i <= length; i++) {
+            let action = 0;
+            if (actionsMap.has(i)) {
+                action = actionsMap.get(i)
+            }
+            if (action !== lastAction) {
+                if (lastAction !== -1) {
+                    actionsList.push([lastAction, (i - lastIndex) / duration / 10]);
+                }
+
+                lastAction = action;
+                lastIndex = i;
+            }
+        }
+
+        if (lastAction !== -1) {
+            actionsList.push([lastAction, (duration * 10 - lastIndex) / duration / 10]);
+        }
+
+        return actionsList;
+    }
+
     function initializeActions(duration) {
         let actionsMap = new Map();
         let length = Math.ceil(duration * 10);
         for (let i = 0; i <= length; i++) {
-            actionsMap.set(i, -1)
+            actionsMap.set(i, 0)
         }
         return actionsMap;
     }
